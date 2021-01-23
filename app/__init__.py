ca9269423437fe0ef1663b17dd3e8348fc6ad8b0
LuanComputacao <eng.luanroger@gmail.com>
2021-01-23 02:29:57 -0300

    feature:#4 Instalando e configurando o Dynaconf

--- app/__init__.py
+++ app/__init__.py
@@ -3,14 +3,14 @@ __version__ = '0.1.0'
 from flask import Flask
 from flask_cors import CORS
 from flask_migrate import Migrate
+from dynaconf import FlaskDynaconf
 
-from config import config_selector
 from app.configs import config_db
 
 
 def create_app(config='production'):
     app = Flask(__name__)
-    app.config.from_object(config_selector[config])
+    FlaskDynaconf(app, settings_files=["settings.toml", ".secrets.toml"])
 
     CORS(app)
 
