a93575d452f932f632b80647f0639d5e6d4b446b
Vinicius Abade <vinicius@kenzie.com.br>
2021-01-23 02:10:54 -0300

    #3 Serializador para village agora consegue processar seus relacionamentos

--- app/schema/__init__.py
+++ app/schema/__init__.py
@@ -1,3 +1,7 @@
 from .village_schema import VillageSchema
 village_schema_list = VillageSchema(many=True)
 village_schema = VillageSchema()
+
+from .ninja_schema import NinjaSchema
+ninja_schema_list = NinjaSchema(many=True)
+ninja_schema = NinjaSchema()
